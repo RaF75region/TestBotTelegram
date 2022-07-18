ba28a1dd79128e028bf49534c3566a8895e01980
al.kolobov <aleks.92@inbox.ru>
2022-07-18 21:43:44 +0700

    first commit

---
+++ Models/MessagerUser.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestBotTelegram.Models
+{
+    public class MessagerUser
+    {
+        public string NameUser{get;set;}
+        public string Message {get;set;}
+        public string ClassName {get;set;}
+    }
+}
